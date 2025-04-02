fa949be173f3facb691fe92c21ce713b5f67696b
Thekeanu <keanu.piras@student.ehb.be>
2025-04-03 01:10:33 +0200

    weergeven van de data op de index

--- javascript/fetch.mjs
+++ javascript/fetch.mjs
@@ -3,17 +3,53 @@
   .then( response => response.json())
   .then(resultaat => {
     // selecteren van de lijst
-    const list = document.getElementById('locations');
+    const list = document.getElementById('locations-lijst');
     console.log(resultaat);
   // inzetten van de data in de lijst
     resultaat.results.forEach(werk => { 
-      const li = document.createElement('li');  
-      li.textContent = `${werk.naam_fresco_nl}`;
-      list.appendChild(li);
+      //aanmaken table row
+      const tableR = document.createElement('tr');
+      //aanmaken table kolom
+      const tableName = document.createElement('td');
+      const tableImage = document.createElement('td');
+      const tableDate = document.createElement('td');
+      const tableDrawer = document.createElement('td');
+      const tableDrawerKunst = document.createElement('td');
+      const tableSurface = document.createElement('td');
+      const tableAdress = document.createElement('td');
+      const tableUitgever = document.createElement('td');
+      const tableFav = document.createElement('td');
+      //gegevens van de api in de table cel zetten
+      tableName.textContent = `${werk.naam_fresco_nl}`;
+      tableDate.textContent = `${werk.date}`;
+      tableDrawer.textContent = `${werk.dessinateur}`;
+      tableSurface.textContent = `${werk.surface_m2}`;
+      tableAdress.textContent = `${werk.adres}`;
+      tableDrawerKunst.textContent = `${werk.realisateur}`;
+      tableUitgever.textContent = `${werk.maison_d_edition}`;
+      const image = document.createElement('IMG');
+      const buttonFav = document.createElement('button');
+      image.src = werk.image;
+      buttonFav.name = `${werk.naam_fresco_nl}`;
+      buttonFav.innerText = "voeg toe aan favorieten";
+      tableFav.appendChild(buttonFav);
+      tableImage.appendChild(image);
+      //toevoegen van cel in de tabel
+      
+      tableR.appendChild(tableImage);
+      tableR.appendChild(tableName);
+      tableR.appendChild(tableDrawer);
+      tableR.appendChild(tableDrawerKunst);
+      tableR.appendChild(tableUitgever);
+      tableR.appendChild(tableDate);
+      tableR.appendChild(tableSurface);
+      tableR.appendChild(tableAdress);
+      tableR.appendChild(tableFav);
+      list.appendChild(tableR);
     });
   })
   .catch(error => {
-    //document.getElementById('error').textContent = `Er ging iets mis: ${error.message}`;
+    document.getElementById('error-message').textContent = `Er ging iets mis: ${error.message}`;
     console.log(error.message);
   });
 
