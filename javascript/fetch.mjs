bf681114b57a50867447a6235830d522cf33f1d4
Thekeanu <keanu.piras@student.ehb.be>
2025-04-02 13:51:25 +0200

    maken van fetch voor data van api en deze ook in de pagina plaatsen

---
+++ javascript/fetch.mjs
@@ -0,0 +1,18 @@
+'use  strict';
+  fetch('https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/bruxelles_parcours_bd/records?limit=2')
+  .then( response => response.json())
+  .then(resultaat => {
+    // selecteren van de lijst
+    const list = document.getElementById('locations');
+    console.log(resultaat);
+  // inzetten van de data in de lijst
+    resultaat.results.forEach(werk => {
+      const li = document.createElement('li');
+      li.textContent = `${werk.naam_fresco_nl}`;
+      list.appendChild(li);
+    });
+  })
+  .catch(error => {
+    //document.getElementById('error-message').textContent = `Er ging iets mis: ${error.message}`;
+    console.log(error.message);
+  });
