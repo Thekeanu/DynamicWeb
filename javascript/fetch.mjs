66e8be88fbb4a9069ed235530ed83bec4bb6965f
Thekeanu <keanu.piras@student.ehb.be>
2025-04-03 18:14:08 +0200

    maken van eventlisteners voor filters

--- javascript/fetch.mjs
+++ javascript/fetch.mjs
@@ -19,6 +19,7 @@ const jaartallen = [];
       
       addFiltersPlaats(werk, gemeentes);
       addFiltersJaar(werk, jaartallen);
+      addFiltersAuteurs(werk, auteurs);
       
       // Aanmaken table row
       const tableR = document.createElement('tr');
@@ -73,9 +74,13 @@ const jaartallen = [];
 
     // Laad favorieten bij opstarten
     showFavorites();
+    //laad filters in 
     showFiltersPlaats();
     showFiltersJaar();
-    
+    showFiltersAuteurs();
+    //wacht voor aanpassingen
+    listenFilters();
+
 
   })
   .catch(error => {
@@ -124,7 +129,7 @@ function showFavorites() {
       tableAdress.textContent = werk.adres;
       tableDrawerKunst.textContent = werk.realisateur;
       tableUitgever.textContent = werk.maison_d_edition;
-        
+        tableAdress.id = werk.commune_gemeente;
         const image = document.createElement('IMG');
         image.src = werk.image;
         image.width = 100;
@@ -161,22 +166,24 @@ function removeFromFavorites(werk) {
 function addFiltersPlaats(werk, gemeentes ){
   const gemeente = werk.commune_gemeente
   if(!gemeentes.some(gem => gemeente === gem)){
-   
     gemeentes.push(gemeente);
-    
   }
 }
 
 function addFiltersJaar(werk, jaartallen){
   const jaartal = werk.date;
-  console.log(jaartal)
   if(!jaartallen.some(jaar => Number(jaartal) === jaar)){
     jaartallen.push(Number(jaartal));
-    console.log(jaartallen);
   }
 }
 
+function addFiltersAuteurs(werk, auteurs ){
+  const auteur = werk.dessinateur;
 
+  if(!auteurs.some(aut => auteur === aut)){
+    auteurs.push(auteur);
+  }
+}
 
 function showFiltersPlaats(){
   const selectElement = document.getElementById('location-filter');
@@ -184,16 +191,14 @@ function showFiltersPlaats(){
   // Voeg voor elke gemeente een option tag toe aan het select element
   gemeentes.forEach(function(gemeente) {
   const option = document.createElement('option');
-  option.addEventListener('click', () => FilterGemeente());
   option.textContent = gemeente;
 
   // Zet de value van de option tag (kan hetzelfde zijn als de tekst)
-  option.value = gemeente.toLowerCase().replace(/\s+/g, '-'); // bijvoorbeeld 'amsterdam'
+  option.value = gemeente; // bijvoorbeeld 'amsterdam'
 
   // Voeg de option toe aan het select element
   selectElement.appendChild(option);
-});
-}
+});}
 
 function showFiltersJaar(){
   const selectElement = document.getElementById('year-filter');
@@ -201,7 +206,6 @@ function showFiltersJaar(){
   // Voeg voor elke gemeente een option tag toe aan het select element
   jaartallen.forEach(function(jaartal) {
   const option = document.createElement('option');
-  option.addEventListener('click', () => Filterjaartal());
   option.textContent = jaartal;
 
   // Zet de value van de option tag (kan hetzelfde zijn als de tekst)
@@ -209,9 +213,91 @@ function showFiltersJaar(){
 
   // Voeg de option toe aan het select element
   selectElement.appendChild(option);
-});
-  
+});}
+
+function showFiltersAuteurs(){
+  const selectElement = document.getElementById('author-filter');
+
+  // Voeg voor elke gemeente een option tag toe aan het select element
+  auteurs.forEach(function(auteur) {
+  const option = document.createElement('option');
+  option.textContent = auteur;
+
+  // Zet de value van de option tag (kan hetzelfde zijn als de tekst)
+  option.value = auteur; // bijvoorbeeld 'amsterdam'
+
+  // Voeg de option toe aan het select element
+  selectElement.appendChild(option);
+});}
+
+function listenFilters() {
+  const locationFilter = document.getElementById('location-filter');
+  locationFilter.addEventListener('change', function() {
+    let selectedValue = locationFilter.value.toLowerCase().trim(); 
+   if (selectedValue != "all") {
+       selectedValue = locationFilter.value.split("-");
+       selectedValue = selectedValue[1].toLowerCase().trim();
+    }
+
+    console.log(selectedValue);
+    const list = document.getElementById('locations-lijst');
+    const rows = list.querySelectorAll('tr');
+    let filteredRows = 0;
+
+    rows.forEach(row => {
+      if(row.style.display === "" || row.style.display === "table-row" || row.style.display == "none" && selectedValue === "all"){
+        const cells = row.querySelectorAll('td');
+      const LKolom = cells[7];
+      if (selectedValue === 'all' || LKolom.textContent.toLowerCase().includes(selectedValue)) {
+        row.style.display = "table-row";
+        filteredRows++;
+      } else {
+        row.style.display = "none";
+      }}
+    });
+
+    // Geef een bericht als er geen gefilterde resultaten zijn
+    if (filteredRows === 0) {
+      const errorMessage = document.getElementById('error-message');
+      errorMessage.textContent = 'Geen resultaten gevonden voor de geselecteerde locatie.';
+    } else {
+      const errorMessage = document.getElementById('error-message');
+      errorMessage.textContent = ''; // Verberg foutmelding als er resultaten zijn
+    }
+  });
+
+  const auteurFilter = document.getElementById('author-filter');
+  auteurFilter.addEventListener('change', function() {
+    let selectedValue = auteurFilter.value.trim();
+    console.log(selectedValue);
+    const list = document.getElementById('locations-lijst');
+    const rows = list.querySelectorAll('tr');
+    let filteredRows = 0;
+
+    rows.forEach(row => {
+      const cells = row.querySelectorAll('td');
+      const LKolom = cells[2];
+      console.log(LKolom.textContent.trim())
+      if (selectedValue === 'all' || LKolom.textContent.trim() === selectedValue) {
+        row.style.display = "table-row";
+        filteredRows++;
+      } else {
+        row.style.display = "none";
+      }
+    });
+
+    // Geef een bericht als er geen gefilterde resultaten zijn
+    if (filteredRows === 0) {
+      const errorMessage = document.getElementById('error-message');
+      errorMessage.textContent = 'Geen resultaten gevonden voor de geselecteerde locatie.';
+    } else {
+      const errorMessage = document.getElementById('error-message');
+      errorMessage.textContent = ''; // Verberg foutmelding als er resultaten zijn
+    }
+  });
 }
+
+
 // is de favorieten sectie
 document.getElementById("show-favorites").addEventListener("click", function() {
   const locationsSection = document.getElementById("locations");
